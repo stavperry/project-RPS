97a11d302576249be6a290e962219083bd26db40
Stav <162802041+stavperry@users.noreply.github.com>
2025-04-05 20:14:57 +0100

    getHumanChoice added and fixed spelling

--- myjavascript.js
+++ myjavascript.js
@@ -1,12 +1,27 @@
+// psuedocode:
+// I want to get a computer choice from rock/paper/or scissors.
+// I want to input either rock/paper/or scissors.
+// I want to compare it to what the computer choice is.
+// if we have the same shape, we have a draw.
+// if either of us has a shape that tramples the other, I want it to declare who won.
+
 function getComputerChoice() {
     let choice = Math.random();
     if (choice <= 0.33) {
-        return shape = "Rock";
+        return shape = "rock";
     } else if ((choice > 0.33) && (choice <= 0.66)) {
-        return shape = "Paper";
+        return shape = "paper";
     } else {
-        return shape = "Scissors";
+        return shape = "scissors";
     }
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice()); /* test that the computer randomises a shape.
+
+function getHumanChoice(myShape) {
+    let userShape = prompt("Enter a shape").toLowerCase();
+    return userShape
+}
+
+// console.log(getHumanChoice()); // checking function getHumanChoice works.
+
